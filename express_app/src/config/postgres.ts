5208e96571d5cae1d4d982dfadd3bc5690099e35
Michał Bazych <bazychm@gmail.com>
2022-04-07 23:39:29 +0200

    update default postgres config for testing

--- express_app/src/config/postgres.ts
+++ express_app/src/config/postgres.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: process.env.PG_HOST || '0.0.0.0',
+  host: process.env.PG_HOST || 'localhost',
   port: parseInt(process.env.PG_PORT || '5432'),
   user: process.env.PG_USERNAME || 'postgres',
   password: process.env.PG_PASSWORD || 'postgres',
