5b6bea23459a9cabaac3559477fb6b19c1e69879
Michał Bazych <bazychm@gmail.com>
2022-04-08 00:47:23 +0200

    update test host to env var

--- express_app/src/tests/postgres-item.js
+++ express_app/src/tests/postgres-item.js
@@ -1,6 +1,6 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = "localhost:8090"
+let server =  process.env.TEST_HOST
 let should = chai.should();
 let expect = chai.expect;
 chai.use(chaiHttp);
